24dcacca480b22d2a74e3f965a11a71bbe47ca35
Sam Page <samnpage96@gmail.com>
2023-11-01 19:44:19 -0400

    changed property names

--- SocialMedia.Data/Entities/CommentsEntity.cs
+++ SocialMedia.Data/Entities/CommentsEntity.cs
@@ -1,17 +1,23 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 
 namespace SocialMedia.Data.Entities;
 
 public class CommentsEntity 
 {
-    public int CommentId { get; set; }
+    [Key]
+    public int Id { get; set; }
+
+    [ForeignKey(nameof(UserEntity))]
     public int UserId { get; set; }
+
+    [ForeignKey(nameof(PostsEntity))]
     public int PostId { get; set; }
 
     [MaxLength(300)]
     public string? Text { get; set; }
-    public DateTime CreatedAt { get; set; }
 
+    public virtual List<PostsEntity> Posts { get; set; } = null!;
 }
