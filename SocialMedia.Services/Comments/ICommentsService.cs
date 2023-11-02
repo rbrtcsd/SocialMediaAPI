ddaa0f7d8b198509bda7c14339080824d6f59cde
Deionta Smith <deiontasmith309@gmail.com>
2023-11-02 00:10:49 -0400

    updated

--- SocialMedia.Services/Comments/ICommentsService.cs
+++ SocialMedia.Services/Comments/ICommentsService.cs
@@ -1,10 +1,10 @@
-// using System;
-// using System.Collections.Generic;
+using System;
+using System.Collections.Generic;
 
-// namespace SocialMedia.Services.Comments
-// {
-//     public interface ICommentsService
-//     {
+namespace SocialMedia.Services.Comments
+{
+    public interface ICommentsService
+    {
      
-//     }
-// }
+    }
+}
