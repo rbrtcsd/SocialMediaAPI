950a159aa9fc3f00669da104e752a5bd6a930579
Deionta Smith <deiontasmith309@gmail.com>
2023-11-02 16:24:09 -0400

    second try

--- SocialMedia.Services/Comments/CommentsService.cs
+++ SocialMedia.Services/Comments/CommentsService.cs
@@ -11,11 +11,17 @@ namespace SocialMedia.Services.Comments
         private readonly ApplicationDbContext _context;
 
         public CommentsService(ApplicationDbContext context)
+        /*It defines a constructor for the CommentsService class that takes an ApplicationDbContext parameter. This constructor is used to inject the database context into the service when an instance of the service is created.*/
         {
             _context = context;
         }
 
+<<<<<<< HEAD
         public async Task<bool> CreateCommentAsync(CommentModel model)
+=======
+        public async Task<bool> CreateCommentAsync(CommentCreate model)
+        /*It defines a method named CreateCommentAsync that takes a CommentCreate model as a parameter and returns a Task<bool>. This method is used to create a new comment in the database.*/
+>>>>>>> 0c7709a (second try)
         {
             var newComment = new Comment
             {
