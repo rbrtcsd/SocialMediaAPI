fe0c361e5d698c4f4d2c0a00cf754303e0cb007b
Sam Page <samnpage96@gmail.com>
2023-11-01 22:36:19 -0400

    added RegisterUser and GetUser

--- SocialMedia.Services/User/IUserService.cs
+++ SocialMedia.Services/User/IUserService.cs
@@ -1,5 +1,8 @@
+using SocialMedia.Models.User;
+
 namespace SocialMedia.Services.User;
 public interface IUserService
 {
-
+    Task<bool> RegisterUserAsync(UserRegister model);
+    Task<UserDetail> GetUserByIdAsync(int userId);
 }
