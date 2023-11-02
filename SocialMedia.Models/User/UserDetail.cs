ea68fe2174ec56b531b5ae6149b2857545394675
Sam Page <samnpage96@gmail.com>
2023-11-01 22:35:58 -0400

    initial commit

---
+++ SocialMedia.Models/User/UserDetail.cs
@@ -0,0 +1,10 @@
+namespace SocialMedia.Models.User;
+public class UserDetail
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = null!;
+    public string UserName { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+}
