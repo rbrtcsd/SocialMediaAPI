6bae89c5065f73f7ecf62031a04376277a9fbf84
Robert Casad <rjcasad@me.com>
2023-11-01 22:04:49 -0400

    Added Post Controller

---
+++ SocialMedia.WebApi/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+
+using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Services.Post;
+
+namespace SocialMedia.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+        private readonly IPostService _postService;
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+    }
+}
