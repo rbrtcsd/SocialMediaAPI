62ade1a5a58105a602f5e000a6aff66d91a5f12d
Sam Page <samnpage96@gmail.com>
2023-11-03 16:25:51 -0400

    updated

--- SocialMedia.Services/Replies/RepliesService.cs
+++ SocialMedia.Services/Replies/RepliesService.cs
@@ -1,13 +1,6 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SocialMedia.Data;
 using SocialMedia.Data.Entities;
 using SocialMedia.Models.Replies;
-using SocialMedia.Services.Replies;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace SocialMedia.Services.Replies;
 
@@ -16,36 +9,29 @@ public class RepliesService : IRepliesService
     private readonly ApplicationDbContext _context;
 
     public RepliesService(ApplicationDbContext context)
-                
+
     {
-      _context = context;
-       
+        _context = context;
+
     }
-        public async Task<bool>CreateReplyAsync(CreateReplies Model)
+    public async Task<bool> CreateReplyAsync(CreateReplies Model)
+    {
+        var newReply = new CreateReplies()
         {
-              var  newReply = new CreateReplies()
-             {
-                Text = Model.Text,
-                CommentsId = Model.CommentsId, 
-             };
+            Text = Model.Text,
+            CommentsId = Model.CommentsId,
+        };
 
-             _context.Replies.Add(newReply); 
-             await _context.SaveChangesAsync();
+        _context.Replies.Add(newReply);
+        await _context.SaveChangesAsync();
 
-             return true;
-        }  
-}
-    public async Task<bool>GetRepliesbyCommentId(int CommentsId)
-    {
-      RepliesEntity? entity = await DbContext.Replies.Find(CommentsId);
-      if (entity is null)
-      return true;
+        return true;
     }
-     
-    
 
-   
-
-
-
-
+    public async Task<bool> GetRepliesbyCommentId(GetReplies model)
+    {
+        RepliesEntity? entity = await _context.Replies.Find(model);
+        if (entity is null)
+            return true;
+    }
+}
